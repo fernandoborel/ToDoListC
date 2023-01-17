c9255b3e245721c35fd2e724f5aa14dafb1f2716
Fernando Borel <fernandomenezesborel@gmail.com>
2023-01-17 12:37:55 -0300

    First Commit

---
+++ ListaDeTarefas/ListaDeTarefas/Models/Tarefas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ListaDeTarefas.Models
+{
+    public class Tarefas
+    {
+        public int TarefasId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Fim { get; set; }
+    }
+}
